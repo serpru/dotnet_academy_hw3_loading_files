c81356fbb06f4d66620b4e42bcff7e95760e47e8
serpru <saviodrg@outlook.com>
2024-01-24 20:59:00 +0100

    Start work on reading files.

---
+++ dotnet_academy_hw3_loading_files/BookInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_academy_hw3_loading_files
+{
+    internal class BookInfo
+    {
+        public List<string> Sentences { get; set; }
+        public List<string> Words { get; set; }
+        public List<string> Punctuation { get; set; }
+        public BookInfo() 
+        {
+            Sentences = new List<string>();
+            Words = new List<string>();
+            Punctuation = new List<string>();
+        }
+
+        public string GetLongestSentenceByChars()
+        {
+            throw new NotImplementedException();
+        }
+        public string GetShortestSentenceByWords()
+        {
+            throw new NotImplementedException();
+        }
+        public string GetLongestWord()
+        {
+            throw new NotImplementedException();
+        }
+        public string GetMostCommonLetter()
+        {
+            throw new NotImplementedException();
+        }
+        public ICollection<string> GetWordsByUseDesc()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- dotnet_academy_hw3_loading_files/Program.cs
+++ dotnet_academy_hw3_loading_files/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var dir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent;
+            var path = String.Concat(dir.FullName, @"\input_data");
+            var bookInfo = new BookInfo();
+
+            try
+            {
+                if (!Directory.Exists(path)) 
+                {
+                    throw new DirectoryNotFoundException();
+                }
+                var fullPath = String.Concat(path, @"\", @"pg100.txt");
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    string line;
+                    string[] words;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var x = line.Select(x => x).Where(x => x.Equals(' ')).ToList();
+                        Console.WriteLine(line);
+                        words = line.Split(" ");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
